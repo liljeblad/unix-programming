b270f39e92f7d73e599fa3164bea823a4731044f
Jesper Liljeblad <jesper.liljeblad@gmail.com>
2014-11-07 15:16:28 +0100

    Added factorial function to functions.s

--- assembly/workbench/functions.s
+++ assembly/workbench/functions.s
@@ -8,17 +8,17 @@ gcd:
 	movl	8(%ebp), %eax
 	movl	12(%ebp), %ebx
 
-if:
+gcdif:
 	cmpl	%eax, %ebx
-	je		done
-	jl		else
+	je		gcddone
+	jl		gcdelse
 	subl	%ebx, %eax
-	jmp		rerun
-else:
+	jmp		gcdrerun
+gcdelse:
 	subl	%eax, %ebx
-rerun:
-	jmp		if
-done:
+gcdrerun:
+	jmp		gcdif
+gcddone:
 	popl	%ebx
 	movl	%ebp, %esp
 	popl	%ebp
@@ -30,6 +30,17 @@ fact:
 	pushl	%ebp
 	movl	%esp, %ebp
 
+	movl	8(%ebp), %ecx
+	movl	$1, %eax
+
+factif:
+	cmpl	$0, %ecx
+	je		factdone
+	mull	%ecx
+	decl	%ecx
+	jmp		factif
+
+factdone:
 	movl	%ebp, %esp
 	popl	%ebp
 	ret
